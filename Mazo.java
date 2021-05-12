f3d0aca00e20ebe010861d1aac9365a7b55a2d9c
Cristopher Gomez Valverde <cristopher.gomez@ucr.ac.cr>
2021-05-11 23:44:46 -0600

    cartas y mazo

---
+++ Mazo.java
@@ -0,0 +1,11 @@
+
+/**
+ * Esta clase construyela carta.
+ * 
+ * @author Cristopher Gomez Valverde B32927 
+ * @version (11/05/2021)
+ */
+public class Mazo
+{
+    
+}
---
+++ cartaletranumero.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+/**
+ * Esta clase construyela carta.
+ * 
+ * @author Cristopher Gomez Valverde B32927 
+ * @version (11/05/2021)
+ */
+public class cartaletranumero
+{
+    char letra;
+    int numero;
+    
+    public cartaletranumero(){
+        Random r = new Random();
+        int i = r.nextInt(26)+65;
+        letra = (char)(i);
+        numero = r.nextInt(10)+1;
+    }
+    
+    public char darLetra(){
+        return letra;
+    }
+    
+    public int darNumero(){
+        return numero;
+    }
+    
+ 
+}
